e2a8d3fe167098de3e10486cdd84ba579856e9ce
=Uladzimir Sapeshka <usapeshka@gmail.com>
2018-09-17 17:32:40 -0500

    Framing string vecotor, func added

--- frame.cpp
+++ frame.cpp
@@ -13,12 +13,31 @@ string::size_type width(const vector<string>& v)
 	return maxlen;
 }
 
+vector<string> frame(const vector<string>& v) {
+	vector<string> ret;
+	string::size_type maxlen = width(v);
+	string border(maxlen+4,'*');
+	
+	// write the top border
+	ret.push_back(border);
+
+	// write each interior row, bordered by an asterisk and a space
+	for (vector<string>::size_type i =0; i!=v.size();++i){
+	 ret.push_back("* " + v[i] + string(maxlen-v[i].size(),' ') +" *");
+	}
+
+	// write the bottom border
+	ret.push_back(border);
+	return ret;
+}
+
 int main() {
 
 	vector<string> v;
-	v.push_back("Hello");
-	v.push_back("123");
-	cout << width(v)<<endl;
+	v.push_back("Sapeshka");
+	vector<string> k = frame(v);
+	for(int i = 0; i!=k.size();++i)
+		cout <<k[i]<<endl; 
 	return 0;
 
 }
